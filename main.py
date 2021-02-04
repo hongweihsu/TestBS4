d92407d144c73a67c14aa9f9b32fa4bcadfcb268
hongweihsu <hongweih@student.unimelb.edu.au>
2021-02-04 14:41:29 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
+print(section_heading.name)
+print(section_heading.string)
+print(section_heading.get("class"))
+
+# CSS selector
+# select_one : 回傳第一個符合的
+# select : 回傳所有符合的
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")  # id = #
+print(name)
+
+headings = soup.select(".heading")  # class = .
+print(headings)
